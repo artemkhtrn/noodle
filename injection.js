35e789fe4e8e800783923069c34b0bb330dcf9d9
artem_khutorian <artem.khutorian@itrium.ru>
2021-01-28 14:34:14 +0300

    update

--- injection.js
+++ injection.js
@@ -2,5 +2,5 @@ var js0 = document.createElement("script");
 js0.src = "https://thecode.media/wp-content/uploads/2019/06/jquery.js";
 document.body.appendChild(js0);
 var js = document.createElement("script");
-js.src = "https://github.com/artemkhtrn/noodle/blob/master/noodle.js";
+js.src = "https://mihailmaximov.ru/projects/lapsha/noodle.js";
 document.body.appendChild(js);
