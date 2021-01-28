3a5af395d63ae5db193247382e5ac9923509b16e
artem_khutorian <artem.khutorian@itrium.ru>
2021-01-28 12:54:57 +0300

    Add JS files

---
+++ injection.js
@@ -0,0 +1,6 @@
+var js0 = document.createElement("script");
+js0.src = "https://thecode.media/wp-content/uploads/2019/06/jquery.js";
+document.body.appendChild(js0);
+var js = document.createElement("script");
+js.src = "https://github.com/artemkhtrn/noodle/blob/master/noodle.js";
+document.body.appendChild(js);
